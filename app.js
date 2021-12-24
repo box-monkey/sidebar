17ed04dc69bffbe160e3289fff95e95484fd9527
box-monkey <jr.gasparin15@gmail.com>
2021-12-24 03:01:04 -0500

    complete

---
+++ app.js
@@ -0,0 +1,18 @@
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+const toggleBtn = document.querySelector(".sidebar-toggle");
+
+toggleBtn.addEventListener("click", function () {
+    console.log(sidebar.classList);
+//   if (sidebar.classList.contains('show-sidebar')) {
+//       sidebar.classList.remove('show-sidebar')
+//   } else {
+//       sidebar.classList.add('show-sidebar');
+//   }
+// above has same functionality as below
+sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+    sidebar.classList.remove("show-sidebar")
+})
